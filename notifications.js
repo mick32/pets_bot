4d22d60ed4e42f07ddcd3084bccd2f23188c2c19
Boris Sobolev <metan32.282@gmail.com>
2019-07-27 22:02:14 +0300

    rename notifications and fix it

---
+++ notifications.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+const token = process.env.TEST_TOKEN;
+const bot = new TelegramBot(token);
+
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://petstestbot.firebaseio.com"
+});
+
+const db = admin.database();
+
+const usersNotifications = async () => {
+  const message =
+    "Хэй, не забывай посмотреть на своих любимых зверушек. Они скучают по тебе =)";
+
+  db.ref("users/").once("value", snap => {
+    const userChatId = Object.keys(snap.val());
+    for (let chatId of userChatId) {
+      try {
+        bot.sendMessage(chatId, "Эй, арбузы пакупай па 4.20");
+      } catch (err) {
+        console.log(
+          "Something went wrong when trying to send a Telegram notification",
+          err
+        );
+      }
+    }
+  });
+};
+
+usersNotifications();
