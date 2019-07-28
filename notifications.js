c8031e2f8de06c2ca75de26affe3378758b7ba41
Boris Sobolev <metan32.282@gmail.com>
2019-07-28 20:21:21 +0300

    temp notifications

--- notifications.js
+++ notifications.js
@@ -13,49 +13,47 @@ admin.initializeApp({
 
 const db = admin.database();
 
-const sendNotifications = () => {
-  const message =
-    "Хэй, не забывай посмотреть на своих любимых зверушек. Они скучают по тебе =)";
-
-  db.ref("users/").once("value", snap => {
-    const userChatId = Object.keys(snap.val());
-    for (let chatId of userChatId) {
-      try {
-        bot.sendMessage(chatId, message);
-      } catch (err) {
-        console.log(
-          "Something went wrong when trying to send a Telegram notification",
-          err
-        );
-      }
-    }
-  });
-};
-
 const currentDate = new Date();
-const currentYear = currentDate.getFullYear();
-const currentMonth = currentDate.getMonth() + 1;
-const currentDay = currentDate.getDay();
+const currentYear = +currentDate.getFullYear();
+const currentMonth = +currentDate.getMonth() + 1;
+const currentDay = currentDate.getDate();
+const message =
+  "Хэй, не забывай посмотреть на своих любимых зверушек. Они скучают по тебе =)";
 
-const sendNotifications1 = async () => {
+const sendNotifications = async () => {
   const requestData = await db.ref("users/").once("value");
   const data = requestData.val();
   let result = [];
+  const arrays = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   for (let user in data) {
     if (data[user].hasOwnProperty("last_usage")) {
       const last_usage = data[user].last_usage.split("/");
-      const userMonth = last_usage[0];
-      const userDay = last_usage[1];
-      const userYear = last_usage[2];
 
-      result.push(data[user].id);
+      const userMonth = +last_usage[0];
+      const userDay = +last_usage[1];
+      const userYear = +last_usage[2];
+
+      const dayDiff = currentDay - userDay >= 2 ? true : false;
+      const monthrDiff = userYear < currentYear ? true : false;
+      const yearDiff =
+        userYear < currentYear && userMonth < currentMonth ? true : false;
+
+      if (monthrDiff || yearDiff) {
+        result.push(data[user].id);
+      } else if (dayDiff) {
+        result.push(data[user].id);
+      }
     }
   }
 
   console.log(result);
+
+  if (result) {
+    for (let chatId of result) {
+      bot.sendMessage("test", "test send");
+    }
+  }
 };
 
-sendNotifications1();
-
-//sendNotifications
+sendNotifications();
