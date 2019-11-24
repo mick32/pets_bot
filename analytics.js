3c99e08a3434e210d1da80188474dbf12ade6fa2
Boris Sobolev <metan32.282@gmail.com>
2019-11-24 14:40:05 +0300

    add import format date

--- analytics.js
+++ analytics.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
+const { format } = require("date-fns");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.DB_URL
